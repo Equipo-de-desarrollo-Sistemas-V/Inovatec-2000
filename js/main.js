3f954c34d5ba9158d56853b8ee4ac36bcfd53d2d
TchRui <rui099098@gmail.com>
2022-09-19 22:29:23 -0500

    Término del diseño y rotacion de la card

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-const tarjeta = document.querySelector('#tarjera')
+const tarjeta = document.querySelector('#tarjeta')
 
-tarjeta.addEventListener('click', () =>{
-    tarjeta.classList.toggle('active');
+	
+// * Volteamos la tarjeta para mostrar el frente.
+const mostrarFrente = () => {
+	if (tarjeta.classList.contains('active')) {
+		tarjeta.classList.remove('active');
+	}
+}
+
+// * Rotacion de la tarjeta
+tarjeta.addEventListener('click', () => {
+	tarjeta.classList.toggle('active');
 });
-
