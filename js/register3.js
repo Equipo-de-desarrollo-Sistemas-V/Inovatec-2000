e09e9db46823d3ec7a941e7ebcda6d5a87745d2b
TchRui <rui099098@gmail.com>
2022-09-20 23:02:11 -0500

    Funcion para el bloqueo del boton

--- js/register3.js
+++ js/register3.js
@@ -15,7 +15,16 @@ const mesExpiracion = document.querySelector('#tarjeta .mes')
 const yearExpiracion = document.querySelector('#tarjeta .year')
 const ccv = document.querySelector('#tarjeta .ccv')
 
-	
+const botonEnviar = document.querySelector('#btn-enviar');
+
+/* Declara una variable global */
+
+let banderaNumero = 0
+let banderaNombre = 0
+let banderaMes = 0
+let banderaYear = 0
+
+
 // * Volteamos la tarjeta para mostrar el frente.
 const mostrarFrente = () => {
 	if (tarjeta.classList.contains('active')) {
@@ -35,54 +44,83 @@ tarjeta.addEventListener('click', () => {
 });
 
 /* Entada y verificacion de la tarjeta */
-formulario.numeroTarjeta.addEventListener('keyup',(e)=>{
+formulario.numeroTarjeta.addEventListener('keyup', (e) => {
 	let valorInput = e.target.value;
 
 	formulario.numeroTarjeta.value = valorInput.replace(/\s/g, '').replace(/\D/g, '').replace(/([0-9]{4})/g, '$1 ').trim();
 
 	numeroTarjeta.textContent = valorInput;
 
-	if(valorInput == ''){
+	if (valorInput == '') {
 		numeroTarjeta.textContent = 'xxxx - xxxx - xxxx - xxxx'
 		logoMarca.innerHTML = ''
 		fondoTarjetaDelantera.style.backgroundImage = 'linear-gradient(45deg, #cdcdcd 5%, #4d4d4d)'
 		fondoTarjetaTrasera.style.backgroundImage = 'linear-gradient(-45deg, #cdcdcd -5%, #4d4d4d)'
+		banderaNumero = 0
 	}
 
-	if(valorInput[0] == 4){
-		logoMarca.innerHTML = '' 
+	if (valorInput[0] == 4) {
+		logoMarca.innerHTML = ''
 		const imagen = document.createElement('img');
 		imagen.src = 'assets/logos-banco/visa.png';
 		logoMarca.appendChild(imagen);
 		fondoTarjetaDelantera.style.backgroundImage = 'linear-gradient(45deg, #5a75e3 5%, #173e7d)';
 		fondoTarjetaTrasera.style.backgroundImage = 'linear-gradient(-45deg, #5a75e3 -5%, #173e7d)';
+
+		if (valorInput.length == 19) {
+			banderaNumero = 1
+		}
+		else {
+			banderaNumero = 0
+		}
 	}
-	else if(valorInput[0] == 5){
+	else if (valorInput[0] == 5) {
 		logoMarca.innerHTML = ''
 		const imagen = document.createElement('img');
 		imagen.src = 'assets/logos-banco/mastercard.png';
 		logoMarca.appendChild(imagen);
 		fondoTarjetaDelantera.style.backgroundImage = 'linear-gradient(45deg, #ea2315 5%, #80140c)';
 		fondoTarjetaTrasera.style.backgroundImage = 'linear-gradient(-45deg, #ea2315 -5%, #80140c)';
+
+		if (valorInput.length == 19) {
+			banderaNumero = 1
+		}
+		else {
+			banderaNumero = 0
+		}
 	}
-	else if(valorInput[0] == 3){
+	else if (valorInput[0] == 3) {
 		logoMarca.innerHTML = ''
 		const imagen = document.createElement('img');
 		imagen.src = 'assets/logos-banco/amex.png';
 		logoMarca.appendChild(imagen);
 		fondoTarjetaDelantera.style.backgroundImage = 'linear-gradient(45deg, #006ecf -5%, #9dd7f5)';
 		fondoTarjetaTrasera.style.backgroundImage = 'linear-gradient(-45deg, #006ecf 5%, #9dd7f5)';
+		if (valorInput.length == 19) {
+			banderaNumero = 1
+		}
+		else {
+			banderaNumero = 0
+		}
 	}
-	else if(valorInput[0] == 6){
+	else if (valorInput[0] == 6) {
 		logoMarca.innerHTML = ''
 		const imagen = document.createElement('img');
 		imagen.src = 'assets/logos-banco/discover.png';
 		logoMarca.appendChild(imagen);
 		fondoTarjetaDelantera.style.backgroundImage = 'linear-gradient(45deg, #c14a0d 5%, #ec9e24)';
 		fondoTarjetaTrasera.style.backgroundImage = 'linear-gradient(-45deg, #c14a0d -5%, #ec9e24)';
-	}	
 
-	mostrarFrente();
+		if (valorInput.length == 19) {
+			banderaNumero = 1
+		}
+		else {
+			banderaNumero = 0
+		}
+	}
+
+	mostrarFrente()
+	/* desbloquearBoton() */
 })
 
 formulario.nombrePropietario.addEventListener('keyup', (e) => {
@@ -90,13 +128,22 @@ formulario.nombrePropietario.addEventListener('keyup', (e) => {
 
 	formulario.nombrePropietario.value = valorInput.replace(/[0-9]/g, '');
 	nombreTarjeta.textContent = valorInput;
-	mostrarFrente();
+	
 
 	firma.textContent = valorInput;
 
-	if(valorInput == ''){
+	if (valorInput == '') {
 		nombreTarjeta.textContent = 'Nombre Y Apellido';
 	}
+
+	if (valorInput.length > 3) {
+		banderaNombre = 1
+	}
+	else {
+		banderaNombre = 0
+	}
+	mostrarFrente()
+	/* desbloquearBoton() */
 })
 
 formulario.mesCaja.addEventListener('keyup', (e) => {
@@ -104,54 +151,64 @@ formulario.mesCaja.addEventListener('keyup', (e) => {
 
 	formulario.mesCaja.value = valorInput.replace(/\s/g, '').replace(/\D/g, '');
 
-	if(valorInput > 12){
+	if (valorInput > 12) {
 		valorInput = 0
 		formulario.mesCaja.value = ""
 		mesExpiracion.textContent = 'MM';
+		banderaMes = 0
 	}
-	else if(valorInput < 1){
+	else if (valorInput < 1) {
 		valorInput = 0
 		formulario.mesCaja.value = ""
 		mesExpiracion.textContent = 'MM'
+		banderaMes = 0
 	}
-	else if(valorInput == ''){
+	else if (valorInput == '') {
 		valorInput = 'MM'
 		formulario.mesCaja.value = 'MM'
+		banderaMes = 0
 	}
 
-	else if(valorInput >= 1 && valorInput < 10){
+	else if (valorInput >= 1 && valorInput < 10) {
 		cadena = '0' + valorInput
 		mesExpiracion.textContent = cadena;
+		banderaMes = 1
 	}
-	else if(valorInput >= 10 && valorInput <= 12){
+	else if (valorInput >= 10 && valorInput <= 12) {
 		mesExpiracion.textContent = valorInput;
+		banderaMes = 1
 	}
 
-	
+	mostrarFrente()
+	/* desbloquearBoton() */
+
 })
 
-formulario.yearCaja.addEventListener('keyup',(e) => {
+formulario.yearCaja.addEventListener('keyup', (e) => {
 	let valorInput = e.target.value;
 
 	formulario.yearCaja.value = valorInput.replace(/\s/g, '').replace(/\D/g, '');
 
-	if(valorInput == ''){
+	if (valorInput == '') {
 		yearExpiracion.textContent = 'YY';
 	}
-	else if(valorInput.length == 2){
+	else if (valorInput.length == 2) {
 
-		if(valorInput >= 15 && valorInput <= 35){
+		if (valorInput >= 15 && valorInput <= 35) {
 			cadena = '20' + valorInput
 			yearExpiracion.textContent = cadena;
+			banderaYear = 1
 		}
-		else{
+		else {
 			valorInput = ''
 			formulario.yearCaja.value = ""
 			yearExpiracion.textContent = 'YY';
+			banderaYear = 0
 		}
-		
 	}
-	
+
+	mostrarFrente()
+	/* desbloquearBoton() */
 })
 
 formulario.ccvCaja.addEventListener('keyup', (e) => {
@@ -159,9 +216,27 @@ formulario.ccvCaja.addEventListener('keyup', (e) => {
 
 	formulario.ccvCaja.value = valorInput.replace(/\s/g, '').replace(/\D/g, '');
 
-	if(valorInput == ''){
+	if (valorInput == '') {
 		ccv.textContent = 'CCV';
 	}
 	ccv.textContent = valorInput;
+
+	if (valorInput.length == 3) {
+		banderaCCV = 1
+	}
+	else {
+		banderaCCV = 0
+	}
 	mostrarTrasera()
+	/* desbloquearBoton() */
 })
+
+
+function desbloquearBoton(){
+	if(banderaNumero == 1 && banderaNombre == 1 && banderaMes == 1 && banderaYear == 1 && banderaCCV == 1) {
+		botonEnviar.style.display = 'flex'
+	}
+	else {
+		botonEnviar.style.display = 'none'
+	}
+}
