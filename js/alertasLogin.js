a1544d7e145d6b80626fff00af427cf45c54fad3
Luis Fernando <LZ20020098@zacatecassur.tecnm.mx>
2022-10-01 10:07:34 -0500

    Detalles del login

--- js/alertasLogin.js
+++ js/alertasLogin.js
@@ -4,17 +4,21 @@ var respuesta = document.getElementById('respuesta');
 formulario.addEventListener('submit', function(e){
     e.preventDefault();
     var datos = new FormData(formulario);
-    fetch('login.php', {
+    fetch('logicaLogin.php', {
         method:'POST',
         body: datos
     })
     .then(res=>res.json())
     .then(data=>{
         if (data==='conn'){
-            alert('Fallo al conectar a la base de datos');
-        }
-        else if(data==='registro'){
-            alert("El correo no se encuentra registrado en el sistema.");
+            alert("Fallo al conectar a la base de datos");
+        } else if(data==='error'){
+            alert("Correo o contraseña invalidos");
+        } else if(data==='registro'){
+            alert("Correo y contraseña no registrados");
+        } else {
+            alert("Iniciado sesion")
+            location.href="login.php";
         }
     })
 })
--- logicaLogin.php
+++ logicaLogin.php
@@ -11,98 +11,96 @@ class login{
         $palabra_secreta = $_POST["password"];
 
         #Instruccion en la base de datos
-        try {
-            $query="SELECT email FROM Empleados WHERE email='$usuario'";
-            $res= sqlsrv_query($conn_sis, $query);
-            if( $res === false) {
+        $query="SELECT email FROM Empleados WHERE email='$usuario'";
+        $res= sqlsrv_query($conn_sis, $query);
+        if( $res === false) {
+            die( print_r( sqlsrv_errors(), true) );
+        }
+        #Inicalizamos un contador con un ciclo para los datos
+        $conta=0;
+        $conta00=0;
+        while( $row = sqlsrv_fetch_array($res) ) {
+            $conta++;
+        }
+        #Se verifica si hay datos de empleados, si no se manda a los clientes
+        if ($conta===1){
+            #Si esta registrado el empleado, se verifica la contraseña
+            $query1="SELECT contra_em FROM Empleados WHERE email='$usuario'";
+            $res1= sqlsrv_query($conn_sis, $query1);
+            if( $res1 === false) {
                 die( print_r( sqlsrv_errors(), true) );
             }
-            #Inicalizamos un contador con un ciclo para los datos
-            $conta=0;
-            while( $row = sqlsrv_fetch_array($res) ) {
-                $conta++;
-            }
-            #Se verifica si hay datos de empleados, si no se manda a los clientes
-            if ($conta===1){
-                #Si esta registrado el empleado, se verifica la contraseña
-                $query1="SELECT contra_em FROM Empleados WHERE email='$usuario'";
-                $res1= sqlsrv_query($conn_sis, $query1);
-                if( $res1 === false) {
-                    die( print_r( sqlsrv_errors(), true) );
-                }
-                $conta1=0;
-                while( $row1 = sqlsrv_fetch_array($res1) ) {
-                    $conta++;
-                    $hash=$row1['contra_em'];
-                    if (password_verify($palabra_secreta, $hash)) {
-                        $query0="SELECT nombres FROM Empleados WHERE email='$usuario'";
-                        $res0= sqlsrv_query($conn_sis, $query0);
-                        if( $res0 === false) {
-                            die( print_r( sqlsrv_errors(), true) );
-                        }
-                        #Inicalizamos un contador con un ciclo para los datos
-                        $conta0=0;
-                        while( $row0 = sqlsrv_fetch_array($res0) ) {
-                            $conta0++;
-                            session_start();
-                            $_SESSION["nombres"] = $row0['nombres'];
-                            include_once "administrativo.php";
-                        }
-                        
-                    } else {
-                        echo 'Invalid password.';
-                    }
-                }
-                #Si hay contraseña entonces ya se entra al sistema, si no el empleado no esta registrado
-            } else{
-                #Si no esta regitrado como cliente, entonces se busca como cliente
-                $usuario = $_POST["email"];
-                $palabra_secreta = $_POST["password"];
-                #Instruccion en la base de datos
-                $query2="SELECT email FROM Persona WHERE email='$usuario'";
-                $res2= sqlsrv_query($conn_sis, $query2);
-                if( $res2 === false) {
-                    die( print_r( sqlsrv_errors(), true) );
-                }
-                $conta2=0;
-                while( $row2 = sqlsrv_fetch_array($res2) ) {
-                    $conta2++;
-                }
-                #Si esta registrado el cliente entonces se verifica junto con la contraseña
-                if ($conta2===1){
-                    $palabra_secreta = $_POST["password"];
-                    $query3="SELECT Contra_us FROM Persona WHERE email='$usuario'";
-                    $res3= sqlsrv_query($conn_sis, $query3);
-                    if( $res3 === false) {
+            $conta1=0;
+            while( $row1 = sqlsrv_fetch_array($res1) ) {
+                $conta1++;
+                $hash=$row1['contra_em'];
+                if (password_verify($palabra_secreta, $hash)) {
+                    $query0="SELECT nombres FROM Empleados WHERE email='$usuario'";
+                    $res0= sqlsrv_query($conn_sis, $query0);
+                    if( $res0 === false) {
                         die( print_r( sqlsrv_errors(), true) );
                     }
-                    $conta3=0;
-                    while( $row3 = sqlsrv_fetch_array($res3) ) {
-                        $conta3++;
-                        $hash1=$row3['Contra_us'];
-                        if (password_verify($palabra_secreta, $hash1)) {
-                            $query011="SELECT nombres FROM Persona WHERE email='$usuario'";
-                            $res011= sqlsrv_query($conn_sis, $query011);
-                            if( $res011 === false) {
-                                die( print_r( sqlsrv_errors(), true) );
-                            }
-                            $conta011=0;
-                            while( $row011 = sqlsrv_fetch_array($res011) ) {
-                                $conta011++;
-                                session_start();
-                                $_SESSION["nombres"] = $row011['nombres'];
-                                include_once "perfilCliente.php";
-                            }
-                        } else {
-                                echo json_encode('registro');
-                            }
+                    #Inicalizamos un contador con un ciclo para los datos
+                    $conta0=0;
+                    while( $row0 = sqlsrv_fetch_array($res0) ) {
+                        $conta0++;
+                        session_start();
+                        $_SESSION["nombres"] = $row0['nombres'];
+                        include_once "administrativo.php";
+            }
+            } else {
+                echo json_encode('error');
+            } 
+            } 
+            #Si hay contraseña entonces ya se entra al sistema, si no el empleado no esta registrado
+        } else if ($conta===0) {
+            #Si no esta regitrado como cliente, entonces se busca como cliente
+            $usuario = $_POST["email"];
+            $palabra_secreta = $_POST["password"];
+            #Instruccion en la base de datos
+            $query2="SELECT email FROM Persona WHERE email='$usuario'";
+            $res2= sqlsrv_query($conn_sis, $query2);
+            if( $res2 === false) {
+                die( print_r( sqlsrv_errors(), true) );
+            }
+            $conta2=0;
+            while( $row2 = sqlsrv_fetch_array($res2) ) {
+                $conta2++;
+            }
+            #Si esta registrado el cliente entonces se verifica junto con la contraseña
+            if ($conta2===1){
+                $palabra_secreta = $_POST["password"];
+                $query3="SELECT Contra_us FROM Persona WHERE email='$usuario'";
+                $res3= sqlsrv_query($conn_sis, $query3);
+                if( $res3 === false) {
+                    die( print_r( sqlsrv_errors(), true) );
                 }
+                $conta3=0;
+                while( $row3 = sqlsrv_fetch_array($res3) ) {
+                    $conta3++;
+                    $hash1=$row3['Contra_us'];
+                    if (password_verify($palabra_secreta, $hash1)) {
+                        $query011="SELECT nombres FROM Persona WHERE email='$usuario'";
+                        $res011= sqlsrv_query($conn_sis, $query011);
+                        if( $res011 === false) {
+                            die( print_r( sqlsrv_errors(), true) );
+                        }
+                        $conta011=0;
+                        while( $row011 = sqlsrv_fetch_array($res011) ) {
+                            $conta011++;
+                            session_start();
+                            $_SESSION["nombres"] = $row011['nombres'];
+                            include_once "perfilCliente.php";
+                        }
+                    } else {
+                        echo json_encode('error');
+                        }
             }
             }
-            }catch (Exception $e){
-                sqlsrv_close($conn_sis);
+            } if ($conta00===0 and $conta===0){
                 echo json_encode('registro');
             }
+            sqlsrv_close($conn_sis);
 }
 }
 $login=new login();
