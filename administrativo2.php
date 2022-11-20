f26682c5e0a93e8651fa488f9bc552421f3ae1fd
Luis Fernando <LZ20020098@zacatecassur.tecnm.mx>
2022-11-20 16:30:54 -0600

    Sesiones

--- administrativo2.php
+++ administrativo2.php
@@ -75,7 +75,7 @@ $sesion_i = $_SESSION["nombres"];
 					<img src="assets-administrativo/Nombre.svg" alt="">
 				</div>
 			</div>
-			<?php echo ucwords("Bienvenido")." ".ucwords($sesion_i);?> 
+			<?php echo ucwords("Bienvenid@")." ".ucwords($sesion_i);?> 
 			<div class="btn-header">
 				<a class="btn-cerrar-session" type="button" href="cerrar.php">Cerrar sesión</a>
 			</div>
--- perfilCliente.php
+++ perfilCliente.php
@@ -112,7 +112,7 @@ else{
             <?php 
              //$_SESSION["Usuario"] = $arreClien['Usuario'];
 			//echo ucwords("Bienvenido") . " " . ucwords($_SESSION['Usuario']);?>
-                    <a class="btn-cerrar-session btn" href="cerrar.php" type="button">Cerrar Sesión</a>
+                    <a class="btn-cerrar-session btn" href="cerrar_cli.php" type="button">Cerrar Sesión</a>
                 <!-- </ul> -->
             </div>
         </nav>
