c5987f36c96270e32619a777380a1723d5bee048
Luis Fernando <LZ20020098@zacatecassur.tecnm.mx>
2022-11-21 13:24:42 -0600

    Sesiones para el index

--- sesion_iniciada_Per.php
+++ sesion_iniciada_Per.php
@@ -4,9 +4,9 @@ session_start();
 $sesion_i = $_SESSION["Usuario"];
 
 if($sesion_i==""){
-    echo'<script>location.href="registroUsuarios.php";</script>';
-}else{
     echo'<script>location.href="index.php";</script>';
+}else{
+    echo'<script>location.href="perfilCliente.php";</script>';
 }
 
 ?>
