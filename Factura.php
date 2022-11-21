62525f098df49cca4e1b5165c747ea50855787c7
Leobardo Daniel Miramontes Murillo <ret.hatsu@gmail.com>
2022-11-21 16:13:15 -0600

    Facturas terminadas

--- Factura.php
+++ Factura.php
@@ -17,7 +17,7 @@ $this->SetFont('Arial','I',8);
 $this->Cell(0,10,'This certificate has been ©  © produced by thetutor',0,0,'R'); 
 } 
 } 
-
+//pruebas item=a%3A2%3A%7Bi%3A0%3Ba%3A2%3A%7Bi%3A0%3Bs%3A4%3A"3050"%3Bi%3A1%3Bs%3A3%3A"820"%3B%7Di%3A1%3Ba%3A2%3A%7Bi%3A0%3Bs%3A1%3A"3"%3Bi%3A1%3Bs%3A1%3A"2"%3B%7D%7D
 $pdf = new FPDF('P','pt','A4'); 
 $arre=$_GET["item"];
 $array_para_recibir_via_url = urldecode($arre);
@@ -70,30 +70,30 @@ $pdf->AddPage();
 //  Print the edge of
 $pdf->Image("Facturas/FacturaImg.png", 20, 20, 540); 
 $pdf->SetFont('Times','',10);
-$pdf->Ln(58);
-$pdf->Cell(355);
+$pdf->Ln(65);
+$pdf->Cell(388);
 $pdf-> Cell(0,13,utf8_decode($nombre),0,0,'L');//nombre
 $pdf->SetFont('Times','',10);
 $pdf->Ln(15);
-$pdf->Cell(355);
+$pdf->Cell(388);
 $pdf->Cell(0,10,utf8_decode($regimen),0,0,'L');//regimen
 $pdf->SetFont('Times','',10);
 $pdf->Ln(22);
-$pdf->Cell(355);
+$pdf->Cell(388);
 $pdf->Cell(0,10,utf8_decode($direccion),0,0,'L');//direccion
 $pdf->Ln(12);
-$pdf->Cell(355);
+$pdf->Cell(388);
 $pdf->Cell(0,10,utf8_decode($cp),0,0,'L');//direccion
 $pdf->SetFont('Times','',10);
 $pdf->Ln(10);
-$pdf->Cell(355);
+$pdf->Cell(388);
 $pdf->Cell(0,10,utf8_decode($telefono),0,0,'L');//telefono
 $pdf->SetFont('Times','',10);
 $pdf->Ln(12);
-$pdf->Cell(355);
+$pdf->Cell(388);
 $pdf->Cell(0,10,utf8_decode($correo),0,0,'L');//correo
 $pdf->SetFont('Times','',10);
-$pdf->Ln(21);
+$pdf->Ln(23);
 $pdf->Cell(385);
 $pdf->Cell(70,10,utf8_decode($precionfinal),0,0,'R');//Deposito
 $pdf->SetFont('Times','',10);
@@ -115,9 +115,9 @@ $pdf->Cell(385);
 $pdf->Cell(70,10,utf8_decode($precionfinal),0,0,'R');//Subtotal
 
 //variable que controla el salto de linea de las ultimas partes
-$saltoLN=386-35;
+$saltoLN=398-38;
 $fecha= date("d/m/Y");
-$pdf->Ln(35);
+$pdf->Ln(38);
 //parte de los productos
 for($i=0;$i<$numPro;$i++) {
     $idProd=$matriz_completa[0][$i];                        //id del producto
@@ -136,11 +136,14 @@ for($i=0;$i<$numPro;$i++) {
     $totProd=$subT-$descuento;
     $pdf->SetFont('Times','',10);
     
-    $pdf->Cell(45);
+    $pdf->Cell(5);
     $pdf->Cell(70,10,utf8_decode($fecha),0,0,'L');//Fecha
     $pdf->SetFont('Times','',10);
-    $pdf->Cell(45);
-    $pdf->Cell(70,10,utf8_decode($nomProd),0,0,'L');//Total debido
+    $pdf->Cell(23);
+    $pdf->Cell(50,10,utf8_decode($cantiProd),0,0,'L');//Fecha
+    $pdf->SetFont('Times','',10);
+    $pdf->Cell(.1);
+    $pdf->Cell(60,10,utf8_decode($nomProd),0,0,'L');//Total debido
     $pdf->SetFont('Times','',10);
     $pdf->Cell(185);
     $pdf->Cell(70,10,utf8_decode($subT),0,0,'L');//Subtotal
