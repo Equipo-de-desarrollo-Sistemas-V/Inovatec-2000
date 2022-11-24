ee5ec450b5f4bc107d72c168c9a6156bf4bfe8cf
naye961 <coral85gemma@hotmail.com>
2022-11-24 13:13:13 -0600

    Barra de busqueda consulta

--- JsPhp/obtenerBusqueda.php
+++ JsPhp/obtenerBusqueda.php
@@ -6,16 +6,24 @@
     include '../conexiones.php';
     $nombre_buscador = $_POST['busqueda'];
 
-    $resultado_productos = sqlsrv_query($conexion, "SELECT productos.Id_producto, productos.nombre, precio_ven, ruta, cantidad
-    FROM $tabla_productos, $tabla_imagenes, $tabla_carrito, $tabla 
-    WHERE (Productos.id_producto like '%".$q."%' or 
-    Productos.nombre like '%".$q."%')
-    
-    
-    Apartado = Apartados.ID_ap and Productos.Subapartado = Id_subap and Apartados.nombre = '$nombre_categoria' and Subapartados.SubApartado = '$nombre_subcategoria' and Productos.id_producto = imagenes.id_prod");
+
+
+    $resultado_productos = sqlsrv_query($conexion, "SELECT Productos.id_producto, Productos.nombre, precio_ven, cantidad, ruta
+    FROM[Productos], [Apartados], [Subapartados], [carritoclientes], [imagenes]
+    WHERE (Productos.id_producto like  '%".$nombre_buscador."%' or 
+    Productos.nombre like '%".$nombre_buscador."%' or 
+    descripcion like '%".$nombre_buscador."%' or
+    Apartados.Nombre like '%".$nombre_buscador."%' or
+    Subapartados.SubApartado like '%".$nombre_buscador."%') and
+	Apartado=Apartados.ID_ap and
+    Productos.Subapartado=Subapartados.Id_subap and 
+	carritoclientes.id_producto=Productos.id_producto and
+	imagenes.id_prod=Productos.id_producto and 
+	usuario= '%".$sesion_i."%'
+    ORDER BY Productos.id_producto");
 
     /* Verficamos que la consulta se haya realizado de manera correcta */
-    if($resultado_computadoras === false){
+    if($resultado_productos === false){
         $mensaje = die(print_r(sqlsrv_errors(), true));
         echo json_encode($mensaje);
     }
@@ -23,19 +31,21 @@
         /* Creamos un arreglo para almacenar los datos de la consulta */
         $nombres_productos = array();
         $precios_productos = array();
-        $categorias_productos = array();
-        $subcategorias_productos = array();
+        //$categorias_productos = array();
+        //$subcategorias_productos = array();
         $rutas_productos = array();
         $id_productos = array();
+        $cantidad_productos = array();
 
         /* Recorremos los datos de la consulta */
-        while($fila = sqlsrv_fetch_array($resultado_computadoras, SQLSRV_FETCH_ASSOC)){
+        while($fila = sqlsrv_fetch_array($resultado_productos, SQLSRV_FETCH_ASSOC)){
             $nombres_productos[] = $fila['nombre'];
             $precios_productos[] = $fila['precio_ven'];
-            $categorias_productos[] = $fila['categoria'];
-            $subcategorias_productos[] = $fila['subcategoria'];
+            //$categorias_productos[] = $fila['categoria'];
+            //$subcategorias_productos[] = $fila['subcategoria'];
             $rutas_productos[] = $fila['ruta'];
             $id_productos[] = $fila['id_producto'];
+            $cantidad_productos[] = $fila['cantidad'];
         }
 
         /* Elimina los dos ulimos digitos a los precios */
@@ -44,7 +54,7 @@
         }, $precios_productos);
 
         /* Creamos un arreglo para almacenar los datos de la consulta */
-        $data = array('nombres' => $nombres_productos, 'precios' => $precios_productos, 'categorias' => $categorias_productos, 'subcategorias' => $subcategorias_productos, 'rutas' => $rutas_productos, 'id_productos' => $id_productos);
+        $data = array('nombres' => $nombres_productos, 'precios' => $precios_productos, 'rutas' => $rutas_productos, 'id_productos' => $id_productos, 'cantidad_productos' => $cantidad_productos);
         echo json_encode($data);
     }
 ?>
