c3ccbc968208673e455c8e2f69b16b00d9d34f2d
Alejandro <Rui099098@gmail.com>
2022-11-28 13:43:12 -0600

    Cambios en el tamaño del facebook

--- JsPhp/logCarritoEliminarProducto.php
+++ JsPhp/logCarritoEliminarProducto.php
@@ -1,9 +1,12 @@
 <?php
     include '../conexiones.php';
+    error_reporting(0);
+    session_start();
+    $usuario = $_SESSION["Usuario"];
 
     $id_producto = $_POST['id'];
 
-    $resultado = sqlsrv_query($conexion, "DELETE FROM $tabla_carrito WHERE id_producto = '$id_producto'");
+    $resultado = sqlsrv_query($conexion, "DELETE FROM $tabla_carrito WHERE id_producto = '$id_producto' and usuario = '$usuario'");
 
     /* Verficamos que la consulta se haya realizado de manera correcta */
     if($resultado === false){
