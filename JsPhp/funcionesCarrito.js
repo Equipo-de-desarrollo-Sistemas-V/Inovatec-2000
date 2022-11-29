bbeea2bbbd588f1e2b5cd5e84d57e9ac66b832c7
Alejandro <Rui099098@gmail.com>
2022-11-29 08:24:25 -0600

    Botones y creación de eliminar todo

--- JsPhp/funcionesCarrito.js
+++ JsPhp/funcionesCarrito.js
@@ -21,3 +21,29 @@ function eliminar(id) {
     console.log(data);
   }
 }
+
+function volver(id){
+  window.location.href = "producto_individual.php?item=" + id
+}
+
+function eliminarTodo(){
+  console.log("Eliminando todo");
+
+  let url = "JsPhp/logCarritoEliminarTodo.php";
+
+  fetch(url, {
+    method: "GET"
+  })
+      .then(response => response.json())
+      .then(data => fetchEjecuted(data))
+      .catch(error => console.log(error));
+
+  const fetchEjecuted = (data) => {
+    let size = Object.keys(data).length;
+    let i = 0;
+
+    for(let i = 0; i < size; i++){
+      document.getElementById(data[i]).remove();
+    }
+  }
+}
