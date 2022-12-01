4694fd21f319d36a6a75a1bb29abed5e11d9fd0f
Alejandro <Rui099098@gmail.com>
2022-12-01 14:03:41 -0600

    Actualización de la ruta en producto_individual

--- producto_individual.php
+++ producto_individual.php
@@ -431,7 +431,7 @@ $sesion_e = $_SESSION["Usuario"];
 
   //Consulta para obtener los datos del productos seleccionado
   $serverName='inovatecserver.database.windows.net';
-  $connectionInfo = array("Database"=>"PagVentas", "UID"=>"usuario", "PWD"=>"123", "CharacterSet"=>"UTF-8");
+  $connectionInfo = array("Database" => "InovatecBD", "UID" => "Inovatecadm", "PWD" => "ProyectoProgramacion5", "CharacterSet" => "UTF-8");
   $con = sqlsrv_connect($serverName, $connectionInfo); 
 
 
