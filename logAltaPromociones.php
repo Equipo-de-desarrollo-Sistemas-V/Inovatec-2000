514140cb1b7f360c3b3bd2c1a8553d1d2174ea06
Ana-Isabel-Garcia <cuentaderespaldogit@gmail.com>
2022-11-30 22:09:44 -0600

    validación del descuento en alta promociones

--- logAltaPromociones.php
+++ logAltaPromociones.php
@@ -27,58 +27,59 @@
                 }
 
                 else{
+                    //verifica que el descuento sea mayor que 0
+                    if($descuento >= 1){
                     //verifica que se haya subido la foto
-                    if ($foto != null and $foto != '') {
-                        //obtener caracteristicas de la imagen
-                        $tipo = $_FILES['foto']['type'];
-                        $tamano = $_FILES['foto']['size'];
-                        $temp = $_FILES['foto']['tmp_name'];
+                        if ($foto != null and $foto != '') {
+                            //obtener caracteristicas de la imagen
+                            $tipo = $_FILES['foto']['type'];
+                            $tamano = $_FILES['foto']['size'];
+                            $temp = $_FILES['foto']['tmp_name'];
 
-                        //comprobar que la extencion del archivo si sea de imagen y no pese mas de 200KB
-                        if (!((strpos($tipo, "gif") || strpos($tipo, "jpeg") || strpos($tipo, "jpg") || strpos($tipo, "png")) && ($tamano < 2000000))) {
-                            echo json_encode("formato");
-                        }
+                            //comprobar que la extencion del archivo si sea de imagen y no pese mas de 200KB
+                            if (!((strpos($tipo, "gif") || strpos($tipo, "jpeg") || strpos($tipo, "jpg") || strpos($tipo, "png")) && ($tamano < 2000000))) {
+                                echo json_encode("formato");
+                            } else {
+                                //guardar la imagen en la carpeta
+                                if (move_uploaded_file($temp, 'imgProm/' . $foto)) {
+                                    //Cambiamos los permisos del archivo a 777 para poder modificarlo posteriormente
+                                    chmod('imgProm/' . $foto, 0777);
 
-                        else{
-                            //guardar la imagen en la carpeta
-                            if (move_uploaded_file($temp, 'imgProm/' . $foto)) {
-                                //Cambiamos los permisos del archivo a 777 para poder modificarlo posteriormente
-                                chmod('imgProm/' . $foto, 0777);
+                                    //ruta de la imagen
+                                    $ruta = 'imgProm/' . $foto;
 
-                                //ruta de la imagen
-                                $ruta = 'imgProm/' . $foto;
+                                    //echo json_encode($ruta);
 
-                                //echo json_encode($ruta);
+                                    //actualiza la tabla productos
+                                    $querry_update = "BEGIN TRAN
+                                    UPDATE Productos
+                                    SET descuento = $descuento  
+                                    WHERE id_producto = $id
+                                    INSERT INTO imgpromocion VALUES($id, '$ruta')
+                                    COMMIT";
 
-                                //actualiza la tabla productos
-                                $querry_update = "BEGIN TRAN
-                                UPDATE Productos
-                                SET descuento = $descuento  
-                                WHERE id_producto = $id
-                                INSERT INTO imgpromocion VALUES($id, '$ruta')
-                                COMMIT";
+                                    $exect_actualizar = sqlsrv_query($con, $querry_update);
 
-                                $exect_actualizar = sqlsrv_query($con, $querry_update);
+                                    //verifica que el descuento si se haya aplicado
+                                    $confirm_prod = sqlsrv_query($con, $querry_descuento);
 
-                                //verifica que el descuento si se haya aplicado
-                                $confirm_prod = sqlsrv_query($con, $querry_descuento);
-
-                                if(sqlsrv_fetch_array($confirm_prod)){
-                                    echo json_encode('todo chido');
-                                }
-
-                                else{
-                                    //el decuento no se aplico bien
-                                    echo json_encode("consulta prod");
+                                    if (sqlsrv_fetch_array($confirm_prod)) {
+                                        echo json_encode('todo chido');
+                                    } else {
+                                        //el decuento no se aplico bien
+                                        echo json_encode("consulta prod");
+                                    }
                                 }
                             }
+                        } else {
+                            //no se subio la foto
+                            echo json_encode("foto nula");
                         }
-
+                    
                     }
 
                     else{
-                        //no se subio la foto
-                        echo json_encode("foto nula");
+                        echo json_encode('descuento');
                     }
                     
                 }
